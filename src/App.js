94dce4a1e3080e9cc63516a1d8fadce2de0b2081
YanaHorbachova <maya69877@gmail.com>
2021-07-04 13:54:29 +0300

    выполнены все здания в HW

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import React from 'react';
 import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions';
+import Notification from './components/Notification';
+import Statistics from './components/Statistics';
 
 class App extends Component {
   state = {
@@ -9,24 +12,49 @@ class App extends Component {
     bad: 0,
   };
 
-  addFeedback = prop => {
+  addFeedback = optionName => {
     this.setState(prevState => {
-      return { [prop]: prevState[prop] + 1 };
+      return { [optionName]: prevState[optionName] + 1 };
     });
+  };
+
+  countTotal = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPercentage = () => {
+    const { good } = this.state;
+    return (good * 100) / this.countTotal();
   };
 
   
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotal();
 
     return (
       <>
         <Section title="Please leave Feedback">
-
+        <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.addFeedback}
+          />
         </Section>
 
-        <Section title="Statistics">
-
-        </Section>
+        {total !== 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercent={this.countPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given." />
+        )}
 
       </>
     );
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,30 @@
+import styles from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+function Statistics({ good, neutral, bad, total, positivePercent }) {
+  return (
+    <div>
+      <p className={styles.input}>Good: {good}</p>
+      <p className={styles.input}>Neutral: {neutral}</p>
+      <p className={styles.input}>Bad: {bad}</p>
+      <p className={styles.input}>Total: {total}</p>
+      <p className={styles.input}>PositivePercent: {positivePercent}%</p>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number,
+  percent: PropTypes.number,
+};
+
+Statistics.defaultProps = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+export default Statistics;
